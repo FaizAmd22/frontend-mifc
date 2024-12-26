99eec51bb09adc8f3bbe4c5ba4a0ce196c1036f6
FaizAmd22 <ahmadfaizhal8@gmail.com>
2024-12-26 10:28:06 +0700

    fix call vct on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Caliper from "./pages/caliper/Caliper";
 import ClutchDisk from "./pages/clutch-disk/ClutchDisk";
-import Vct from "./pages/vct/vct";
+import Vct from "./pages/vct/Vct";
 import Layout from "./layout/layout";
 import NotFound from "./pages/notFound/notFound";
 
